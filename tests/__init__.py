8e4e9cfb0c95d07c2da025acca0b58b3937af667
Martin Bierbaum <martin.bierbaum@rub.de>
2020-02-18 18:53:30 +0100

    show request information in test app

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
-from flask_tex import render_to_pdf
+from flask_tex import render_to_pdf, TeX
 
 
 app = Flask(__name__)
+TeX(app)
 
 
 @app.route("/")
 def index():
-    return render_to_pdf("test.tex", foo="Hello World")
+    return render_to_pdf("index.tex", foo="Hello World")
