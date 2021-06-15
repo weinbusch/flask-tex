683d2b96a66183dd985a9dac7708859bf621c2e5
Jan Wille <jan.wille@stud.hs-hannover.de>
2021-06-15 21:42:31 +0200

    fixed problem with temp-dir on different drive on windows

--- flask_tex/__init__.py
+++ flask_tex/__init__.py
@@ -60,9 +60,9 @@ def run_tex(source, command, directory):
     filename = "texput.tex"
     with open(os.path.join(directory, filename), "x", encoding="utf-8") as f:
         f.write(source)
-    args = f'cd "{directory}" && {command} -interaction=batchmode {filename}'
+    args = f'{command} -interaction=batchmode {filename}'
     try:
-        run(args, shell=True, stdout=PIPE, stderr=PIPE, check=True)
+        run(args, cwd=directory, shell=True, stdout=PIPE, stderr=PIPE, check=True)
     except CalledProcessError:
         try:
             with open(os.path.join(directory, "texput.log"), "r") as f:
